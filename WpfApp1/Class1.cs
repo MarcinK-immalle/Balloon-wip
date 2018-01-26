47c4492505dbc7cd6345c6df8267938f33ba5dcf
Marcin Kostulski <marcin.kostulski@B209-01>
2018-01-26 11:04:35 +0100

    meh

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    class Baloon
+    {
+
+        private int x;
+        private int y;
+        private int diameter;
+        private Ellipse ellipse;
+
+        public Baloon()
+        {
+            CreateEllipse();
+            UpdateEllipse();
+        }
+
+        public void MoveRight(int xStep)
+        {
+
+        }
+
+        public void ChangeSize(int change)
+        {
+
+        }
+
+        public void DisplayOn(Canvas drawingCanvas)
+        {
+
+        }
+
+        private void CreateEllipse()
+        {
+
+        }
+
+        private void UpdateEllipse()
+        {
+
+        }
+    }
+
+    
+}
